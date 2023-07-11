cfe70df136995ea2ea46ed08aedc3bd52c54bce8
Ariel <luisvega@uagrm.edu.bo>
2023-07-11 13:49:50 -0400

    add: agrega proyecto de prueba de UI

---
+++ EjemploUI/src/ejemploui/EjemploUI.java
@@ -0,0 +1,17 @@
+
+package ejemploui;
+
+/**
+ *
+ * @author vluis
+ */
+public class EjemploUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
